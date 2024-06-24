1f87c92aeb0d25c91afa0207255e9b70d870fe01
Bruno valdagno <brunovaldagno@gmail.com>
2024-06-24 19:30:46 -0300

    atualizacao do curso

---
+++ Exercicio Mysql/exercicio para treinar.sql
@@ -0,0 +1,93 @@
+/* Criando o banco de dados*/
+
+create database oficina;
+
+/* entrando no banco de dados */
+
+use oficina;
+
+/* Criando tabelas*/
+
+create table cliente(
+	idcliente int primary key auto_increment,
+	nome varchar(30) not null,
+	sexo enum('M','F') not null,
+	id_carro int unique
+
+);
+
+create table telefone(
+	idtelefone int primary key auto_increment,
+	tipo enum ('CEL','RES','COM') not null,
+	numero varchar(30) not null,
+	id_cliente int	
+);
+
+
+
+create table marca(
+	idmarca int primary key auto_increment,
+	marca varchar(30) unique
+);
+
+
+create table carro(
+	idcarro int primary key auto_increment,
+	modelo varchar(30) not null,
+	placa varchar(30) not null unique,
+	id_marca int
+);
+
+
+create table cor(
+	idcor int primary key auto_increment,
+	cor varchar(30) unique
+);
+
+
+create table carro_cor(
+	id_carro int,
+	id_cor int,
+	primary key(id_carro,id_cor)
+);
+
+
+
+/* Constraints*/
+
+alter table telefone
+add constraint fk_telefone_cliente
+foreign key(id_cliente)
+references cliente(idcliente);
+
+alter table cliente
+add constraint fk_cliente_carro
+foreign key(id_carro)
+references carro(idcarro);
+
+
+alter table carro 
+add constraint fk_carro_marca
+foreign key(id_marca)
+references marca(idmarca);
+
+
+alter table carro_cor
+add constraint fk_cor
+foreign key(id_carro)
+references carro(idcarro);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
